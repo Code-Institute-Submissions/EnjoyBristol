73545d802665c3bc6c57c30c6a9deaa7de1de37b
svickery <s.vickery@hotmail.co.uk>
2021-02-18 13:04:42 +0000

    EmailJS added to html file and javacript added

---
+++ assets/javascript/sendEmail.js
@@ -0,0 +1,31 @@
+/*function sendMail(contactForm)  {
+    emailjs.send("service_jgyzbkn","enjoybristol"; {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value,
+    });
+    .then(
+        function(response)  {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+        return false;
+}*/
+
+function sendMail(contactForm)  {
+    emailjs.send ("service_jgyzbkn", "enjoybristol", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "your_message": contactForm.projectsummary.value,
+    });
+    then (
+        function(response)  {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+        return false;
+}
